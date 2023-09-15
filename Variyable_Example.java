7c2123a6a2b66ce69cc6634b4d5b6bf83d761848
srilathas9 <130989512+srilathas9@users.noreply.github.com>
2023-09-15 15:07:42 +0530

    Day 1 Assignment

---
+++ Variyable_Example.java
@@ -0,0 +1,21 @@
+package com.coreJava;
+
+public class Variyable_Example {
+
+	public static void main(String[] args) {
+		
+		int num1 = 12;
+		int num2 =13;
+	boolean result = num1 == num2;
+	
+	if (result) {
+	System.out.println("num1 num2 values are equal");
+	}
+	else
+	{
+		System.out.println("num1 num2 values are not equal");
+	}
+	
+	}
+
+}
