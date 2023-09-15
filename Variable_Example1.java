26f9d0aa60cd50839ac6565cf82622f6392a75de
srilathas9 <130989512+srilathas9@users.noreply.github.com>
2023-09-15 16:08:50 +0530

    Day 1 Assignment

---
+++ Variable_Example1.java
@@ -0,0 +1,27 @@
+package com.coreJava;
+
+public class Variable_Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int num1 = 12;
+		int num2 = 13;
+		int num3 = 11;
+
+		int sum  = num1 + num2 + num3;
+		int sub =  num1 - num2 - num3;
+		int avg = (num1+num2+num3)/3;		
+		int mult = num1*num2*num3;
+				
+		System.out.println(sum);
+		System.out.println(sub);
+		System.out.println(avg);
+		System.out.println(mult);
+	}
+	
+
+}
+
+
